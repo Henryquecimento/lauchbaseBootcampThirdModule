e6c863f514f9941e5c10ab7c0951702232c55c39
Henryque <henryque_1@hotmail.com>
2020-09-07 12:58:01 -0300

    third module exercise files

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: 'K-8z_4xvT3o',
+        title: "Recreating Twitter's interface | Rocketseat",
+        duration: "2 h 08 min",
+        price: "Free",
+        featured: true,
+    },
+    {
+        id: 'EhnXaybirdA',
+        title: "Create your own project from zero to your portfolio | Rocketseat",
+        duration: "00 h 48 min",
+        price: "Free",
+    },
+    {
+        id: '2alg7MQ6_sI',
+        title: "GIT and GITHUB in pratice | Rocketseat",
+        duration: "1 h 45 min",
+        price: "Free",
+        featured: true,
+    },
+    {
+        id: 'GSqR2i-Pq6o',
+        title: "Who is the 'THIS' variable? | Rocketseat",
+        duration: "1 h 08 min",
+        price: "Free",
+    },
+    {
+        id: '2nXsLpUCO20',
+        title: "Recreating Instagram's interface | Rocketseat",
+        duration: "00 h 58 min",
+        price: "Free",
+    },
+    {
+        id: 'p6d3RzhgQbA',
+        title: "Creating Templates | Rocketseat",
+        duration: "00 h 48 min",
+        price: "Free",
+        featured: true,
+    },
+]
